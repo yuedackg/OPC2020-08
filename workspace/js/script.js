34d193d1188de01f42b98da0670d60b477d3cbbb
Yoshihiro UEDA <y.ueda@ckg.ac.jp>
2020-08-21 15:20:54 +0900

    2020-0820-1520

---
+++ workspace/js/script.js
@@ -0,0 +1,40 @@
+// ----------------------------------------------
+// ▼設定：スライドショーで見せたい画像ファイル群
+// ----------------------------------------------
+var imgset = [
+    "imgs/yaiba1.jpg"        ,
+    "imgs/yaiba2.jpg"  ,
+    "imgs/yaiba3.jpg"  ,
+    "imgs/yaiba4.jpg"  ,
+    "imgs/yaiba5.jpg"  ,
+    "imgs/yaiba6.jpg"  
+ ];
+// -----------------------------------------------
+// ▼関数A：指定画像を順に表示させる
+// -----------------------------------------------
+var counter = 0;
+function slideimage() {
+   if( counter >= imgset.length ) {
+      // カウンタが画像数よりも大きくなったら0番に戻す
+      counter = 0;
+   }
+   // 対象要素の画像URLを次の画像のURLに切り替える
+   document.getElementById('slideshow').src = imgset[counter];
+   // カウンタを1増やす
+   counter++;
+}
+// -----------------------------------------------
+// ▼関数B：スライドショーを制御
+// -----------------------------------------------
+var slideid = 0;
+function startstopshow() {
+   if( slideid == 0 ) {
+      // 始まっていなければ始める
+      slideid = setInterval(slideimage,1000);   // 1000は切替秒数(ミリ秒)
+   }
+   else {
+      // IDがあれば止める
+      clearInterval(slideid);
+      slideid = 0;
+   }
+}
